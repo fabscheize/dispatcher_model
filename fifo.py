6832197c3a8e0ef27fab6c7493a4e50f526bb1df
Vlad Karsten <vkarsten1111@icloud.com>
2024-05-17 18:32:31 +0700

    2

--- fifo.py
+++ fifo.py
@@ -1,9 +1,5 @@
 from enum import Enum
 
-RAM = 16
-HDD = 12
-Km = 0
-
 
 class Status(Enum):
     created = 0
@@ -12,7 +8,7 @@ class Status(Enum):
     ended = 3
     released = 4
 
-class Resourses():
+class Resources():
     def __init__(self):
         self.ram = 16
         self.hdd = 12
@@ -31,9 +27,11 @@ class Resourses():
         self.multitask -= 1
 
     def seize(self, required_ram, required_hdd):
-        self.ram += required_ram
-        self.hdd += required_hdd
+        self.ram -= required_ram
+        self.hdd -= required_hdd
 
+    def print(self):
+        print(f'\nВремя: {self.t:3d}. Свободные ресурсы: ОП = {self.ram:2d}/16, ВП = {self.hdd:2d}/12, Km = {self.multitask:2d}')
 
 class Task():
     def __init__(self, task_id=0, required_ram=0, required_hdd=0, receipt_time=0, input_time=0, execution_time=0):
@@ -47,59 +45,50 @@ class Task():
         self.status = None
         self.time_to_exec = None
 
-    def execute(self):
-        global Km
-        global RAM
-        global HDD
-        self.time_to_end -= 1 / Km
+    def execute(self, resources: Resources):
+        self.time_to_end -= 1 / resources.multitask
         if self.time_to_end <= 1e-6:
             self.status = Status.ended
-            RAM += self.required_ram
-            HDD += self.required_hdd
-            Km -= 1
+            resources.release(self.required_ram, self.required_hdd)
             return Status.ended
         return None
 
 
-    def input(self, current_time):
-        global Km
+    def input(self, resources: Resources):
         if self.time_to_exec is None:
-            self.time_to_exec = current_time + self.input_time
-        if self.time_to_exec is current_time:
+            self.time_to_exec = resources.t + self.input_time
+        if self.time_to_exec is resources.t:
             self.status = Status.inputed
-            Km += 1
-            self.execute()
+            resources.add_task()
+            self.execute(resources)
             return Status.inputed
         if self.status is Status.created:
             return None
         self.status = Status.created
         return Status.created
 
-    def create(self, current_time):
-        global RAM
-        global HDD
-        if self.receipt_time is current_time:
-            if self.required_ram > RAM or self.required_hdd > HDD:
+    def create(self, resources: Resources):
+        if self.receipt_time == resources.t:
+            if self.required_ram > resources.ram or self.required_hdd > resources.hdd:
                 self.status = Status.in_queue
                 return Status.in_queue
             else:
-                RAM -= self.required_ram
-                HDD -= self.required_hdd
-                return self.input(current_time)
+                resources.seize(self.required_ram, self.required_hdd)
+                return self.input(resources)
         else:
             return None
 
-    def check(self, current_time):
+    def check(self, resources: Resources):
         if self.status is Status.ended:
             return None
         if self.status is Status.inputed:
-            return self.execute()
+            return self.execute(resources)
         if self.status is (Status.created or Status.released):
-            return self.input(current_time)
+            return self.input(resources)
         if self.status is Status.in_queue:
             ...
 
-        return self.create(current_time)
+        return self.create(resources)
 
 
 def print_task_info(ret, task: Task):
@@ -121,14 +110,10 @@ def print_task_info(ret, task: Task):
     return ended
 
 def main():
-    global Km
-    t = 0
     ended = 0
     print_data = False
-    # Km = 0
-    # ram = 16
-    # hdd = 12
 
+    resources = Resources()
     task_1 = Task(1, 3,	2,	4,	10,	60)
     task_2 = Task(2, 5,	0,	9,	0,	30)
     task_3 = Task(3, 1,	3,	18,	15,	20)
@@ -138,23 +123,23 @@ def main():
     task_7 = Task(7, 4,	1,	27,	5, 10)
     task_8 = Task(8, 4,	1,	30,	5, 10)
     task_9 = Task(9, 4,	1,	33,	5, 10)
-    task_10 = Task(10, 6,	2,	33,	10,	60)
+    task_10 = Task(10, 6, 2, 33, 10, 60)
 
     tasks = [task_1, task_2, task_3, task_4, task_5,
              task_6, task_7, task_8, task_9, task_10]
 
     while ended < 10:
         for task in tasks:
-            ret = task.check(t)
+            ret = task.check(resources)
             if ret is not None:
                 print_data = True
             ended += print_task_info(ret, task)
 
         if print_data is True:
-            print(f'\nВремя: {t}. Свободные ресурсы: ОП = {RAM:2d}/16, ВП = {HDD:2d}/12, Km = {Km:2d}')
+            resources.print()
             print('------------------------------')
             print_data = False
-        t += 1
+        resources.clock()
 
     # a = Status.created
     # print(f'{a is Status.created}')
